4e21e03ebbafe10b412f263fbf1e8dd2c6dfae44
Zair Montoya <z.montoya@uniandes.edu.co>
2026-05-12 06:29:43 -0500

    parcial

--- mi-api/src/calc/dto/circle-query.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNumber, Min } from 'class-validator';
-import { Type } from 'class-transformer';
-
-export class CircleQueryDto {
-  @Type(() => Number)   // string → number (query params)
-  @IsNumber()
-  @Min(0)
-  radius: number;
-}
